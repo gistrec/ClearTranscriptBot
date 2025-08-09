6ec2a04927e2cd926a3ca1cf3b505e41ce147f5c
Aleksandr Kovalko <gistrec@gmail.com>
2025-08-09 21:32:36 +0500

    Add MySQL connection setup

---
+++ database.py
@@ -0,0 +1,25 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+MYSQL_USER     = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST     = os.getenv("MYSQL_HOST")
+MYSQL_PORT     = os.getenv("MYSQL_PORT")
+MYSQL_DB       = os.getenv("MYSQL_DB")
+
+# Как можно скачать сертификат для подключения к MySQL
+# mkdir ~/.mysql
+# curl -o ~/.mysql/root.crt https://storage.yandexcloud.net/cloud-certs/CA.pem
+ssl_ca_path = os.path.expanduser("~/.mysql/root.crt")
+assert os.path.isfile(ssl_ca_path), "Не найден сертификат для подключения к MySQL"
+
+engine = create_engine(f"mysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}?ssl_ca={ssl_ca_path}", pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Разогреваем пул, чтобы не было задержек при первом подключении
+with engine.connect() as connection:
+    pass
